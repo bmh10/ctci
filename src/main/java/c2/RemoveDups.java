7609ef289613c3ce726d3a5825a17ccd496d3dc9
bmh10 <bmhomer13@gmail.com>
2016-04-09 16:12:24 +0100

    Remove Dups (part 2)

--- src/main/java/c2/RemoveDups.java
+++ src/main/java/c2/RemoveDups.java
@@ -8,9 +8,9 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Set;
 
-
 public class RemoveDups {
 
+    // Time O(n) Space O(n)
     public void removeDups(LinkedList list) {
         Set seen = new HashSet();
 
@@ -25,18 +25,46 @@ public class RemoveDups {
         }
     }
 
+    // Time O(n^2) Space O(1)
+    public void removeDupsNoBuffer(LinkedList list) {
+        Iterator it = list.iterator();
+        int idx = 0;
+
+        while (it.hasNext()) {
+            Object o = it.next();
+            // Use same iterator object for inner iterator
+            // to avoid concurrent modification exception
+            it = list.listIterator(idx + 1);
+
+            while (it.hasNext()) {
+                Object innerO = it.next();
+                if (o.equals(innerO)) {
+                    it.remove();
+                }
+            }
+
+            idx++;
+            it = list.listIterator(idx);
+        }
+    }
+
     @Test
     public void test() {
-        final LinkedList<Integer> A = new LinkedList<Integer>() {{
+        final LinkedList<Integer> A1 = new LinkedList<Integer>() {{
             add(3); add(3); add(4); add(1); add(5); add(4); add(1);
         }};
+
+        final LinkedList<Integer> A2 = new LinkedList<Integer>(A1);
 
         final LinkedList<Integer> Aans = new LinkedList<Integer>() {{
             add(3); add(4); add(1); add(5);
         }};
 
-        removeDups(A);
-        Assert.assertEquals(Aans, A);
-    }
 
+        removeDups(A1);
+        Assert.assertEquals(Aans, A1);
+
+        removeDupsNoBuffer(A2);
+        Assert.assertEquals(Aans, A2);
+    }
 }
