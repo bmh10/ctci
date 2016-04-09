06bcb8e9b5609f859acd71b9470953cc6447e9b3
bmh10 <bmhomer13@gmail.com>
2016-04-09 20:12:28 +0100

    SumLists (part 2)

--- src/main/java/c2_linkedLists/SumLists.java
+++ src/main/java/c2_linkedLists/SumLists.java
@@ -8,11 +8,12 @@ import java.util.LinkedList;
 
 public class SumLists {
 
+    // Time O(N) Space O(N)
     public LinkedList<Integer> sumListsReverse(LinkedList<Integer> l1, LinkedList<Integer> l2) {
         final LinkedList<Integer> res = new LinkedList<Integer>();
-        int carry = 0;
         final Iterator<Integer> it1 = l1.iterator();
         final Iterator<Integer> it2 = l2.iterator();
+        int carry = 0;
 
         while (it1.hasNext() || it2.hasNext()) {
             int v1 = it1.hasNext() ? it1.next() : 0;
@@ -25,8 +26,34 @@ public class SumLists {
         return res;
     }
 
+    // Time O(N) Space O(N)
+    public LinkedList<Integer> sumListsForwards(LinkedList<Integer> l1, LinkedList<Integer> l2) {
+        Iterator<Integer> it = l1.iterator();
+        StringBuilder sb = new StringBuilder();
+        while (it.hasNext()) {
+            sb = sb.append(it.next());
+        }
+
+        int sum = Integer.parseInt(sb.toString());
+
+        it = l2.iterator();
+        sb = new StringBuilder();
+        while (it.hasNext()) {
+            sb = sb.append(it.next());
+        }
+
+        sum += Integer.parseInt(sb.toString());
+
+        final LinkedList<Integer> res = new LinkedList<Integer>();
+        for (char c : Integer.toString(sum).toCharArray()) {
+            res.addLast(Integer.parseInt(c + ""));
+        }
+
+        return res;
+    }
+
     @Test
-    public void test() {
+    public void test_sumListsReverse() {
         final LinkedList<Integer> A = new LinkedList<Integer>() {{
             add(7); add(1); add(6); add(3);
         }};
@@ -40,5 +67,22 @@ public class SumLists {
         }};
 
         Assert.assertEquals(ans, sumListsReverse(A, B));
+    }
+
+    @Test
+    public void test_sumListsForwards() {
+        final LinkedList<Integer> A = new LinkedList<Integer>() {{
+            add(3); add(6); add(1); add(7);
+        }};
+
+        final LinkedList<Integer> B = new LinkedList<Integer>() {{
+            add(2); add(9); add(5);
+        }};
+
+        final LinkedList<Integer> ans = new LinkedList<Integer>() {{
+            add(3); add(9); add(1); add(2);
+        }};
+
+        Assert.assertEquals(ans, sumListsForwards(A, B));
     }
 }
